a913aadaf9c7592dd39ff15753a3d3ac1d5927d6
Nicole Dominguez <sodevious.net@gmail.com>
2013-08-19 11:24:19 -0400

    recurly js

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,6 +1,3 @@
-$(document).ready(function(){
-  $("a.login").click(function () {
-    $(".nav").hide();
-    $(".loginBox").fadeIn();
-  });
+$(function() {
+	$('a[rel*=leanModal]').leanModal({ top : 20 });		
 });
